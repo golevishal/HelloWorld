84be1a3df4ef728ff6385972b2bb1f2280d96cfb
golevishal <golevishal08@gmail.com>
2019-07-28 16:54:18 -0700

    Java 11 work

---
+++ src/com/experiments/Java11StringWork.java
@@ -0,0 +1,17 @@
+package com.experiments;
+
+public class Java11StringWork {
+
+    public static void main(String[] args) {
+        // Repeat String
+        System.out.println("Hello".repeat(2));
+
+        // Is Blank ??
+        System.out.println("Hello".isBlank());
+        System.out.println("".isBlank());
+
+        // Is Empty ??
+        System.out.println("Hello".isEmpty());
+        System.out.println("".isEmpty());
+    }
+}
